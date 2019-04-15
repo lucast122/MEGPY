fd62e9713d055df16d98a9f209a48c461113db93
lucass122 <timo-niklas.lucas@student.uni-tuebingen.de>
2019-04-15 16:08:23 +0200

    fixed grouped barplot colors and labeling

--- analysis/Taxonomy/taxonomy_plot.py
+++ analysis/Taxonomy/taxonomy_plot.py
@@ -1,36 +1,44 @@
-"""
- *  Copyright (C) 2019 Timo Niklas Lucas
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+#  """
+#   *  Copyright (C) 2019 Timo Niklas Lucas
+#   *
+#   *  This program is free software: you can redistribute it and/or modify
+#   *  it under the terms of the GNU General Public License as published by
+#   *  the Free Software Foundation, either version 3 of the License, or
+#   *  (at your option) any later version.
+#   *
+#   *  This program is distributed in the hope that it will be useful,
+#   *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   *  GNU General Public License for more details.
+#   *
+#   *  You should have received a copy of the GNU General Public License
+#   *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#  """
+
 import argparse
 import glob
 import ntpath
 import os
+import random
 from collections import OrderedDict
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 parser = argparse.ArgumentParser(description='Parse command line arguments.')
 parser.add_argument('-t', '--threshold', type=float,
                     help='Float value for plotting cutoff. Only classes with higher '
                          'percentage than this value are shown')
 
-parser.add_argument('-p', '--plot', action='store_true',
-                    help='If set MEGAN is not run and just the plot is created')
+parser.add_argument('-e', '--extract', action='store_true',
+                    help='Provide this parameter to extract the taxon_to_percent data from the provided .daa files using MEGAN'
+                         'for this to work MEGAN ultimate has to be installed and ')
+
 parser.add_argument('--donut', action='store_true',
-                    help='If set only the donut plots will be created ')
+                    help='Provide this parameter to create the donut plots ')
+
+parser.add_argument('--bar', action='store_true',
+                    help='Provide this parameter to create the grouped bar plots')
 
 parser.add_argument('-d', '--daa', type=str,
                     help='Path to the folder containing the .daa files of interest')
@@ -39,6 +47,12 @@ parser.add_argument('-o', '--output', type=str,
                     help='Path to output folder')
 
 args = parser.parse_args()
+
+"""
+CUSTOMIZE MEGAN_COMMAND:
+YOU MUST CHANGE THE PATH TO MEGAN ULTIMATE TO FIT YOUR SYSTEM
+"""
+MEGAN_COMMAND = "/Applications/MEGAN_/MEGAN.app/Contents/MacOS/JavaApplicationStub -g -E < "
 
 # Threshold in percent for plotting. Every taxonomic class with a percentage higher than this is used for the plot
 threshold = args.threshold
@@ -53,49 +67,28 @@ phyla = []
 phyla_combined = []
 values = []
 pos = []
+temp_bars = []
 data_bars = []
 phyla_pairwise = []
 data_pairwise = []
 temp = []
+xticks_pos = [0]
 temp2 = []
-
+data_donut_plots = []
+labels_donut_plots = []
+daa_file_names = []
 # Create commands.txt with the proper commands for the right file
 
 # For loop over all .daa files in folder specified by mapping_file_path
-# Creates a specific command.txt for each .daa file present
-for file in glob.glob(mapping_file_path + "*.daa"):
-    mapping_file_name = ntpath.basename(file)
-    print("Current mapping file: " + mapping_file_name)
-
-    with open("Output/" + mapping_file_name + '_commands.txt', 'w') as the_file:
-        count_file_name = str(mapping_file_name) + "_taxon_to_percent.txt"
-        count_file_absolute_path = count_folder_path + count_file_name
-        the_file.write("open viewer=Taxonomy;\n")
-        the_file.write("open file='" +
-                       str(file) + "';\n")
-
-        the_file.write("uncollapse nodes = all;\n")
-
-        the_file.write("select rank=Order;\n")
-        the_file.write("select nodes=subtree;\n")
-        the_file.write("export what=CSV format=taxonName_to_percent separator=tab counts=assigned file='" + out_path +
-                       str(count_file_absolute_path) + "';\n")
-        the_file.write("quit;")
-        the_file.close()
-
-        command = "/Applications/MEGAN_/MEGAN.app/Contents/MacOS/JavaApplicationStub -g -E < " + \
-                  "Output/" + str(mapping_file_name) + "_commands.txt"
-    if not args.plot:
-        try:
-            os.system(command)
-        except OSError:
-            print("Failed to start MEGAN. Please configure the correct path")
+# Creates a specific command.txt for each .daa file present,
 
 # find all taxon_to_percent  .txt
 # files in the taxon_to_percent folder
 # and use data for plotting. For loop to iterate over all files
 
-for file in glob.glob("Output/" + "*taxon_*.txt"):
+
+for count, file in enumerate(glob.glob("Output/" + "*taxon_*.txt"), 1):
+    daa_file_names.append(count)
     phyla = []
     values = []
     phyla_with_percentage = []
@@ -104,8 +97,6 @@ for file in glob.glob("Output/" + "*taxon_*.txt"):
 
     file_ID = os.path.splitext(file_name)[0]
     full_path = count_folder_path + file_name
-
-    print("Creating taxonomic plot for file " + file_ID)
 
     with open(full_path, "r") as f:
         content = f
@@ -137,139 +128,150 @@ for file in glob.glob("Output/" + "*taxon_*.txt"):
         c += 1
 
     # add category to plot that represents everything below threshold
-
     phyla_with_percentage.append("Other " + "[" + str(values[-1]) + "%]")
     phyla.append("Other")
-
     phyla_combined += phyla
-
     data = values
     data_bars += values
+    temp_bars += values
+
+    data_bars.append(0)
+
+    phyla_pairwise.append(phyla)
+    data_pairwise.append(values)
+
+    data_donut_plots.append(data)
+    labels_donut_plots.append(phyla_with_percentage)
+
+
+# call important methods
+
+
+def main():
+    if args.extract:
+        extract_taxon_to_percent()
+    if args.donut:
+        create_donut_plots()
+    if args.bar:
+        create_grouped_barplot(data_bars)
+
+
+def extract_taxon_to_percent():
+    for file in glob.glob(mapping_file_path + "*.daa"):
+        mapping_file_name = ntpath.basename(file)
+        print("Current mapping file: " + mapping_file_name)
+
+        with open("Output/" + mapping_file_name + '_commands.txt', 'w') as the_file:
+            count_file_name = str(mapping_file_name) + "_taxon_to_percent.txt"
+            count_file_absolute_path = count_folder_path + count_file_name
+            the_file.write("open viewer=Taxonomy;\n")
+            the_file.write("open file='" +
+                           str(file) + "';\n")
+
+            the_file.write("uncollapse nodes = all;\n")
+
+            the_file.write("select rank=Order;\n")
+            the_file.write("select nodes=subtree;\n")
+            the_file.write(
+                "export what=CSV format=taxonName_to_percent separator=tab counts=assigned file='" + out_path +
+                str(count_file_absolute_path) + "';\n")
+            the_file.write("quit;")
+            the_file.close()
+
+            command = MEGAN_COMMAND + \
+                      "Output/" + str(mapping_file_name) + "_commands.txt"
+        if not args.plot:
+            try:
+                os.system(command)
+            except OSError:
+                print("Failed to start MEGAN. Please configure the correct path")
+
+
+def create_donut_plots():
     recipe = phyla_with_percentage
     fig, ax = plt.subplots(figsize=(16, 10), subplot_kw=dict(aspect="equal"))
-
-    # wedges, texts = ax.pie(data, wedgeprops=dict(width=0.5), startangle=-40)
+    print("Creating taxonomic plot for file " + file_ID)
+    wedges, texts = ax.pie(data, wedgeprops=dict(width=0.5), startangle=-40)
     #
     bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
     #
     kw = dict(xycoords='data', textcoords='data', arrowprops=dict(arrowstyle="-"),
               bbox=bbox_props, zorder=0, va="center")
-    #
-    # for i, p in enumerate(wedges):
-    #     ang = (p.theta2 - p.theta1) / 2. + p.theta1
-    #     y = np.sin(np.deg2rad(ang))
-    #     x = np.cos(np.deg2rad(ang))
-    #     horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
-    #     connectionstyle = "angle,angleA=0,angleB={}".format(ang)
-    #     kw["arrowprops"].update({"connectionstyle": connectionstyle})
-    #     ax.annotate(recipe[i], xy=(x, y), xytext=(1.35 * np.sign(x), 1.4 * y),
-    #                 horizontalalignment=horizontalalignment, **kw)
-    #
-    # # Title messses picture up. Use latex fig title instead
-    # # ax.set_title("Distribution of taxa on phylum level")
-    # try:
-    #     fig.savefig(count_folder_path + file_ID + ".pdf", bbox_inches='tight')
-    #     print("Plot successfully created for " + str(file_ID))
-    #     plt.close()
-    #     pass
-    # except Exception as e:
-    #     print("Could not create plot successfully.")
-    #     plt.close()
-    # else:
-    #     pass
-    # finally:
-    #     pass
+
+    for i, p in enumerate(wedges):
+        ang = (p.theta2 - p.theta1) / 2. + p.theta1
+        y = np.sin(np.deg2rad(ang))
+        x = np.cos(np.deg2rad(ang))
+        horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+        connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+        kw["arrowprops"].update({"connectionstyle": connectionstyle})
+        ax.annotate(recipe[i], xy=(x, y), xytext=(1.35 * np.sign(x), 1.4 * y),
+                    horizontalalignment=horizontalalignment, **kw)
+
+    # Title messses picture up. Use latex fig title instead
+    # ax.set_title("Distribution of taxa on phylum level")
+    try:
+        fig.savefig(count_folder_path + file_ID + ".pdf", bbox_inches='tight')
+        print("Plot successfully created for " + str(file_ID))
+        plt.close()
+        pass
+    except Exception as e:
+        print("Could not create plot successfully.")
+        plt.close()
+    else:
+        pass
+    finally:
+        pass
 
 
-#     phyla_pairwise.append(phyla)
-#     data_pairwise.append(values)
-#
-# for phylum_list in phyla_pairwise:
-#     for phylum_list2 in phyla_pairwise:
-#         # print(phylum_list)
-#         # print(phylum_list2)
-#         temp = []
-#         if phylum_list == phylum_list2:
-#             continue
-#
-#         temp.append(phylum_list)
-#         temp.append(phylum_list2)
-#         temp2.append(temp)
-#
-# print(temp2)
-# phyla_pairwise = temp2
-#
-# temp2 = []
-# for dat in data_pairwise:
-#     for dat2 in data_pairwise:
-#         temp = []
-#         if dat == dat2:
-#             continue
-#         temp.append(dat)
-#         temp.append(dat2)
-#         temp2.append(temp)
-#
-# data_pairwise = temp2
-
-
-# start 2nd plot
-
-
-def grouped_barplot():
-    pos_count = 0
+def create_grouped_barplot(data):
+    # sns.set_palette(sns.color_palette("hls", 20))
+    pos_count = 3
     print("Creating grouped bar chart ...")
-    bar_width = 1
-    # plt.bar(pos, data, bar_width, color='blue', edgecolor='black')
-    for dat in data_bars:
+    bar_width = 3
+
+    for dat in data:
+        if dat == 0:
+            pos_count += 20
+            xticks_pos.append(pos_count)
+            continue
         pos.append(pos_count)
-        pos_count += 1.1
-    data_int = [round(int(float(i))) for i in data_bars]
+        pos_count += 6
+
+    dat = temp_bars
+
+    data_int = [round(int(float(i))) for i in dat]
 
     keys = range(len(data_int))
-    phyla_dict = OrderedDict(zip(keys, phyla_combined))
+    phyla_dict = dict(zip(keys, phyla_combined))
     phyla_non_redundant = list(dict.fromkeys(phyla_combined))
-    # create dictionary mapping names to colors
-    my_colors = [(x / 200,
-                  (x / len(phyla_non_redundant)), 0.5) for x in range(len(phyla_non_redundant))]
-    color_dict = OrderedDict(zip(phyla_non_redundant, my_colors))
-    print(color_dict)
-    # prop_cycle = plt.rcParams['axes.prop_cycle']
-    # colors = prop_cycle.by_key()['color']
-    print(len(data_bars))
-
-    barlist = []
-    # for counter, _ in enumerate(data_bars):
-    # print("Plotting bar " + str(counter) + " / " + str(len(data_int)))
-    # plt.cla()
-    # lab = phyla_dict[counter]
-    # col = color_dict[lab]
+    # create dictionary mapping taxon labels to colors
+    my_colors = [colors(n)[0] for n, _ in enumerate(phyla_non_redundant, 1)]
+    color_dict = dict(zip(phyla_non_redundant, my_colors))
     bar_container = plt.bar(pos, height=data_int, width=bar_width)
+    # set bar color according to taxon class
 
+    labels = []
     for counter, b in enumerate(bar_container):
         lab = phyla_dict[counter]
+        labels.append(lab)
         b.set_label(lab)
         col = color_dict[lab]
         b.set_color(col)
+    # labels = list(dict.fromkeys(labels))
+    # remove duplicates from legend
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = OrderedDict(zip(labels, handles))
 
-        # print(lab)
-        # print(bar.get_children())
-        # for rect in bar.get_children():
-        #     col = color_dict[lab]
-        #     rect.set_color(col)
-
-    # for bar in barlist:
-    #     for l in bar.get_children():
-    #         l.set_color(color_dict[bar.get_label()])
-    #         print(color_dict[bar.get_label()])
-    # handles, labels = plt.gca().get_legend_handles_labels()
-    # by_label = OrderedDict(zip(labels, handles))
-    # plt.legend(phyla_non_redundant)
-    plt.legend(phyla_non_redundant, bbox_to_anchor=(1.04, 1), loc="upper left")
-
-    plt.xticks()
+    # create legend
+    plt.legend(by_label.values(), by_label.keys(), bbox_to_anchor=(1.04, 1), loc="upper left")
+    # plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
+    print(daa_file_names)
+    plt.xticks(xticks_pos, daa_file_names)
     plt.margins(x=0.03)
-    plt.xlim(0)
-    plt.xlabel('Taxonomic Class', fontsize=12)
+
+    plt.xlim(1)
+    plt.xlabel('Dataset ID', fontsize=12)
     plt.ylabel('Relative abundance in percent', fontsize=12)
     plt.title('Taxonomic analysis of the dataset ' + str(mapping_file), fontsize=12)
 
@@ -281,5 +283,25 @@ def grouped_barplot():
     plt.close()
 
 
-if not args.donut:
-    grouped_barplot()
+def colors(n):
+    if n == 0:
+        return
+    ret = []
+    r = int(random.random() * 256)
+    g = int(random.random() * 256)
+    b = int(random.random() * 256)
+    assert n != 0
+    step = 256 / n
+    for i in range(n):
+        r += step
+        g += step
+        b += step
+        r = (int(r) % 256) / 256
+        g = (int(g) % 256) / 256
+        b = (int(b) % 256) / 256
+        ret.append((r, g, b))
+        return ret
+
+
+if __name__ == '__main__':
+    main()
