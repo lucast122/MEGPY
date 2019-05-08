6cedc60ff4b08ab8c07f51c07e4237c8f230991e
lucass122 <timo-niklas.lucas@student.uni-tuebingen.de>
2019-05-08 15:00:54 +0200

    fixed a few bugs gc_assembly_map now works improved taxonomy plot quality

--- analysis/Find_Strains/gc_assembly_map.py
+++ analysis/Find_Strains/gc_assembly_map.py
@@ -19,6 +19,23 @@ Dehydrogenae. It then takes the resulting contigs and uses NCBI-BLAST to find ou
 the SEED attribute. The output is a list of organisms with the desired SEED attribute
 """
 
+#  """
+#   *  Copyright (C) 2019 Timo Niklas Lucas
+#   *
+#   *  This program is free software: you can redistribute it and/or modify
+#   *  it under the terms of the GNU General Public License as published by
+#   *  the Free Software Foundation, either version 3 of the License, or
+#   *  (at your option) any later version.
+#   *
+#   *  This program is distributed in the hope that it will be useful,
+#   *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   *  GNU General Public License for more details.
+#   *
+#   *  You should have received a copy of the GNU General Public License
+#   *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#  """
+
 import argparse
 import ntpath
 import os
@@ -37,6 +54,7 @@ class TimeoutException(Exception):  # Custom exception class
 def timeout_handler(signum, frame):  # Custom signal handler
     raise TimeoutException
 
+
 E_VALUE_THRESH = 0.04
 
 GC_OUTPUT = "gc_temp"
@@ -50,7 +68,6 @@ args = parser.parse_args()
 
 MAPPING_FILE_PATH = args.input
 MAPPING_FILE_NAME = ntpath.basename(MAPPING_FILE_PATH)[:-5]
-
 
 """
 Performs Gene Centric assembly for a specific SEED ID on a given .daa file
@@ -85,7 +102,10 @@ def blast(input, max_time):
 
     for record in SeqIO.parse(input, "fasta"):
         # Only BLAST contigs that came from more than 20 reads and have more than 50bp
-        if len(record.seq) < 50 or int(record.description.split(" ")[2].split("=")[1]) < 20:
+        if len(record.seq) < 50 or int(record.description.split(" ")[2].split("=")[1]) < 20 \
+                or float(record.description.split()[-1].split("=")[1].strip("]")) < 7:
+            print(
+                "Skipping contig, because either sequence is too short, or coverage is too low / not enough base pairs")
             continue
 
         print("Running BLAST for record ", record.id, record.seq)
@@ -95,23 +115,23 @@ def blast(input, max_time):
         # This try/except loop ensures that
         #   you'll catch TimeoutException when it's sent.
         try:
-            result_handle = Bio.Blast.NCBIWWW.qblast("blastn", "nr", record.seq, megablast=True)
+            result_handle = Bio.Blast.NCBIWWW.qblast("blastx", "nr", record.seq, megablast=True)
+
             with open("blast_output_" + MAPPING_FILE_NAME + args.id + ".xml", "w") as out_handle:
                 out_handle.write(result_handle.read())
 
             result_handle.close()
         except TimeoutException:
             print("BLAST run aborted, because it took longer than " + str(max_time) + " seconds.")
-            with open(BLAST_OUTPUT_NAME, "a") as writer:
-                writer.writelines("BLAST failed. Runtime exceeded maximum time")
-
+            # out_handle.writelines("BLAST failed. Runtime exceeded maximum time")
             continue  # continue the for loop if function A takes more than defined number of seconds
         else:
             # Reset the alarm
             signal.alarm(0)
 
         # Now write the BLAST output to a file and create another file with a list of strains
-        blast_record = NCBIXML.read(result_handle)
+        handle = open("blast_output_" + MAPPING_FILE_NAME + args.id + ".xml", "r")
+        blast_record = NCBIXML.read(handle)
         with open(BLAST_OUTPUT_NAME, "a") as writer:
             for alignment in blast_record.alignments:
                 strains.append(alignment.title)
@@ -129,25 +149,27 @@ def blast(input, max_time):
     return strains
 
 
-
 # Run the gene centric assembly using the command line parameters
 gc_assembly_file_name = GC_OUTPUT + "_ID_" + args.id + ".fasta"
 # gc_assembly(args.input, gc_assembly_file_name, args.id)
 
 # Run blast with the gc assembly output vs nr
-strains = blast(gc_assembly_file_name, 60)
+print(gc_assembly_file_name)
+strains = blast(gc_assembly_file_name, 300)
 strains = list(dict.fromkeys(strains))
 
 # Now write all unique strains associated with wanted MEGAN ID to a file
 STRAINS_FILE_NAME = "strains_" + str(MAPPING_FILE_NAME) + args.id
+
+# delete duplicate strains
+strains = list(dict.fromkeys(strains))
+#write strains to file
 with open(STRAINS_FILE_NAME, "w") as f:
     for strain in strains:
         f.writelines(strain)
+        f.write("\n")
 
 # create summary file to informs about present strains as well as strains associated with reverse-beta-oxidation
-
-
-
 
 
 # blast("reads-Acyl-CoA_dehydrogenase__short-chain_specific__EC_1.3.99.2.fasta")
